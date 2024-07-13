3ce4c78b5b73e80f51f07ac91cab8befaf725302
Pavan Gurram <pavang_hcl@yahoo.com>
2024-07-13 20:40:40 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webapp03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
